b0a14c585f0d5c0882d3fec8f0211db7532abae4
abbimdeleve <adeleve1024@yahoo.com>
2019-09-24 09:26:03 -0400

    Updates

---
+++ precache-manifest.5d7f2c749c7a88d5844f1ba15618780c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ab77f15d9dcafafb6ee865ef740ee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef4b49b7fea9830d3efe",
+    "url": "/static/css/main.56c0df75.chunk.css"
+  },
+  {
+    "revision": "bf7883e6f1f4eaea9ca1",
+    "url": "/static/js/2.e8619310.chunk.js"
+  },
+  {
+    "revision": "ef4b49b7fea9830d3efe",
+    "url": "/static/js/main.88307844.chunk.js"
+  },
+  {
+    "revision": "8a54b91ca4a2f802d977",
+    "url": "/static/js/runtime-main.d32eaf10.js"
+  }
+]);
